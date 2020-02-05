6d7d722d5e68ebc7e5e8dbe94610ee8858ee1782
tsk-nkgw <tasktas9@gmail.com>
2020-02-05 22:22:55 +0900

    create ast

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/FileAST.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/FileAST.kt
@@ -3,4 +3,4 @@ package jp.ac.osaka_u.ist.sdl.ops.ast
 import org.eclipse.jdt.core.dom.CompilationUnit
 import java.nio.file.Path
 
-data class FileAST(val ast: CompilationUnit, val path: Path)
+data class FileAST(val path: Path, val ast: CompilationUnit)
