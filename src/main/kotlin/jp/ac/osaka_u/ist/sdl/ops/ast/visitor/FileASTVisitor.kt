f2e42ad2ad88501ec38d46d61e111ab2b823941b
T45K <tasktas9@gmail.com>
2020-02-12 11:30:03 +0900

    add constructor

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/visitor/FileASTVisitor.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/visitor/FileASTVisitor.kt
@@ -1,5 +1,6 @@
 package jp.ac.osaka_u.ist.sdl.ops.ast.visitor
 
+import com.github.javaparser.ast.body.ConstructorDeclaration
 import com.github.javaparser.ast.body.MethodDeclaration
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter
 import jp.ac.osaka_u.ist.sdl.ops.ast.FileAST
@@ -21,6 +22,26 @@ class FileASTVisitor(private val fileAST: FileAST) : VoidVisitorAdapter<Void?>()
 
         val methodVisitor = MethodVisitor(node.parameters)
         node.body.get().accept(methodVisitor, null)
+        method.order = methodVisitor.orders
+                .map { it.value }
+                .sortedBy { it.declared }
+                .map { it.referenced }
+        methods.add(method)
+    }
+
+    override fun visit(node: ConstructorDeclaration, arg: Void?) {
+        if (node.body.isEmpty) {
+            return
+        }
+
+        val method = Method(fileAST.path, node.name.identifier, true, node.parameters.size)
+        if (node.parameters.size <= 1) {
+            methods.add(method)
+            return
+        }
+
+        val methodVisitor = MethodVisitor(node.parameters)
+        node.body.accept(methodVisitor, null)
         method.order = methodVisitor.orders
                 .map { it.value }
                 .sortedBy { it.declared }
