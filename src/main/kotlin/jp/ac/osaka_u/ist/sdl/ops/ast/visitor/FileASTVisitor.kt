59007035ef1d0c0f9c5e20d4d16c935352d7584c
T45K <tasktas9@gmail.com>
2020-02-12 11:10:14 +0900

    modify

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/visitor/FileASTVisitor.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/ops/ast/visitor/FileASTVisitor.kt
@@ -1,40 +1,36 @@
 package jp.ac.osaka_u.ist.sdl.ops.ast.visitor
 
+import com.github.javaparser.ast.body.MethodDeclaration
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter
 import jp.ac.osaka_u.ist.sdl.ops.ast.FileAST
 import jp.ac.osaka_u.ist.sdl.ops.entity.Method
-import org.eclipse.jdt.core.dom.ASTVisitor
-import org.eclipse.jdt.core.dom.MethodDeclaration
-import org.eclipse.jdt.core.dom.SingleVariableDeclaration
 
-class FileASTVisitor(private val fileAST: FileAST) : ASTVisitor() {
+class FileASTVisitor(private val fileAST: FileAST) : VoidVisitorAdapter<Void>() {
     val methods: MutableList<Method> = mutableListOf()
 
-    override fun visit(node: MethodDeclaration): Boolean {
-        if (node.body == null || node.body.statements().isEmpty()) {
-            return false
+    override fun visit(node: MethodDeclaration, arg: Void) {
+        if (node.body.isEmpty || node.body.get().isEmpty) {
+            return
         }
 
-        val method = Method(fileAST.path, node.name.identifier, node.isConstructor, node.parameters().size)
-        if (node.parameters().size <= 1) {
+        val method = Method(fileAST.path, node.name.identifier, false, node.parameters.size)
+        if (node.parameters.size <= 1) {
             methods.add(method)
-            return false
+            return
         }
 
-        @Suppress("UNCHECKED_CAST")
-        val methodVisitor = MethodVisitor(node.parameters() as List<SingleVariableDeclaration>)
-        node.body.accept(methodVisitor)
+        val methodVisitor = MethodVisitor(node.parameters)
+        node.body.get().accept(methodVisitor, null)
         method.order = methodVisitor.orders
                 .map { it.value }
                 .sortedBy { it.declared }
                 .map { it.referenced }
         methods.add(method)
-
-        return false
     }
 }
 
 fun takeOutMethods(fileAST: FileAST): List<Method> {
     val visitor = FileASTVisitor(fileAST)
-    fileAST.ast.accept(visitor)
+    fileAST.ast.accept(visitor, null)
     return visitor.methods
 }
